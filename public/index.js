bda5a789bdcfdd8421b40c4d1436ec23f8a34324
John Aspinall <john.aspinall73@gmail.com>
2025-07-01 09:07:01 +0100

    Added click even listeners to score increment buttons

---
+++ public/index.js
@@ -0,0 +1,38 @@
+
+let homeDigit = document.getElementById('home-digit');
+let guestDigit = document.getElementById('guest-digit');
+
+function homeAddOne() {
+   console.log('Home team adds one point');
+}
+function homeAddTwo() {
+   console.log('Home team adds two points');
+}
+function homeAddThree() {
+   console.log('Home team adds three points');
+}
+function guestAddOne() {
+   console.log('Guest team adds one point');
+}
+function guestAddTwo() {
+   console.log('Guest team adds two points');
+}
+function guestAddThree() {
+   console.log('Guest team adds three points');
+}
+
+let homeOneBtn = document.getElementById('home-one');
+let homeTwoBtn = document.getElementById('home-two');
+let homeThreeBtn = document.getElementById('home-three');
+
+let guestOneBtn = document.getElementById('guest-one');  
+let guestTwoBtn = document.getElementById('guest-two');
+let guestThreeBtn = document.getElementById('guest-three');
+
+console.log(homeOneBtn);
+console.log(homeTwoBtn);
+console.log(homeThreeBtn);
+console.log(guestOneBtn);
+console.log(guestTwoBtn);
+console.log(guestThreeBtn);
+
