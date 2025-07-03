77ccd693661387cfa9ce888d71888b459648c778
John Aspinall <john.aspinall73@gmail.com>
2025-07-03 12:42:36 +0100

    Stretch goal 1 completed, indicating whos winning with a crown

--- public/index.js
+++ public/index.js
@@ -1,9 +1,11 @@
 
 // File: src/index.js
-
-// Get references to the score digit elements               
+           
 let homeDigitEl = document.getElementById('home-digit');
 let guestDigitEl = document.getElementById('guest-digit');
+
+let homeTitleEl = document.getElementById('home-title');
+let guestTitleEl = document.getElementById('guest-title');
 
 // Convert the text content of the elements to numbers for calculations
 let homeDigitValue = Number(homeDigitEl.textContent);
@@ -19,29 +21,51 @@ function resetScores() {
    console.log(homeDigitValue, guestDigitValue);
 }
 
-// Functions to increment the home and guest scores
+function checkLeader(){
+   if (homeDigitValue > guestDigitValue) {
+      guestTitleEl.textContent = 'Guest'
+      homeTitleEl.textContent = 'Home 👑'
+   }
+   if (guestDigitValue > homeDigitValue) {
+      guestTitleEl.textContent = 'Guest'
+      guestTitleEl.textContent = 'Guest 👑'
+   }
+   else if (guestDigitValue === homeDigitValue) {
+      guestTitleEl.textContent = 'Guest'
+      homeTitleEl.textContent = 'Home'
+      console.log('Its a draw')
+   }
+}
+
+// Count scores and check for leader
 function homeAddOne() {
-   homeDigitValue ++
+   homeDigitValue ++;
    homeDigitEl.textContent = homeDigitValue;
+   checkLeader();
 }
 function homeAddTwo() {
    homeDigitValue += 2;
    homeDigitEl.textContent = homeDigitValue;
+   checkLeader();
 }
 function homeAddThree() {
    homeDigitValue += 3;
    homeDigitEl.textContent = homeDigitValue;
+   checkLeader();
 }
 function guestAddOne() {
    guestDigitValue ++;
    guestDigitEl.textContent = guestDigitValue;
+   checkLeader();
 }
 function guestAddTwo() {
    guestDigitValue += 2;
    guestDigitEl.textContent = guestDigitValue;
+   checkLeader();
 }
 function guestAddThree() {
    guestDigitValue += 3;
    guestDigitEl.textContent = guestDigitValue;
+   checkLeader();
 }
 
